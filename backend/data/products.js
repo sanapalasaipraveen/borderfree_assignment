45f69c253b6a4ba751164429d02fcb6ae916f095
sanapalasaipraveen <saipraveensanapala776@gmail.com>
2022-04-05 00:07:50 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Sennheiser",
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQl6L9vFQ2INp14-FD3iKN9xZY1krNdujs9ZUBli0Ge2DOvRv_SPdnaEC2bfUJFCjdGuD7Go1HE6X8&usqp=CAc",
+    description:
+      "Sennheiser HD4.40 Blueooth Headphone with Mic, Black",
+    price: 5499,
+    countInStock: 15,
+  },
+  {
+    name: "Tag Hueuer",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQqljzqLiCqGcFQC6dtvAc1XEJfDPSnB4aXR2hfWGAreaqTK-2RFWVIx3Stasi4cA&usqp=CAc",
+    description:
+      "Branded Watch Carrera Calibre 17 Black",
+    price: 10000,
+    countInStock: 10,
+  },
+  {
+    name: "OnePlus 9 Pro",
+    imageUrl:
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ9ARLO2oWo6Ldm9XFZadzFxxWYZS_Oqv1dXmnPqSvXrjgbDB8Z51OxmuP939vOE5eS8Bd5AerGEoEY-sGq0En162TPxGc_IQ",
+    description:
+      "OnePlus 9 Pro 5G (Morning Mist, 12GB RAM, 256GB Storage)",
+    price: 70000,
+    countInStock: 5,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    description:
+      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+    price: 4000,
+    countInStock: 25,
+  },
+  {
+    name: "Audio Technica Headphones",
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSKoJEI1tAYx0JumuYljrB6936-UBHCp_MJYSJHx_wStWQcsLEl2TgwH9JcuGYm&usqp=CAc",
+    description:
+      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
+    price: 11000,
+    countInStock: 4,
+  },
+  {
+    name: "JBL FLIP 4",
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRg4rrF1UjYivlXukyPHWI7nnO6kkF8fQOMSU3lzy9-Z6O8KXNhLbf844U5ZnAzv4Rc1JcvY4saug0&usqp=CAc",
+    description:
+      "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
+    price: 8000,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
